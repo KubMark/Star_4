18cc22a5207516e5725a4801b9557019e91b8870
KubMark <mypur86@yahoo.com>
2022-07-24 14:22:22 +0300

    Initial commit

---
+++ 1_TimeTask.py
@@ -0,0 +1,10 @@
+time = input('Введите звездочки и точки ')
+hour = 0
+minute = 0
+for symbol in time:
+    if symbol == '*':
+        hour += 1
+    else:
+        minute = int(minute)
+        minute += 15
+print(f'{hour}:{minute}')
---
+++ Task 5.py
@@ -0,0 +1,12 @@
+#               Задание 5
+free_mailbox = ['yandex.ru', 'mail.ru', 'gmail.com', 'yahoo.com', 'rambler.ru']
+user_mail = input('Вводим почту ')
+
+user_mailbox = user_mail.split('@')[1]
+
+if '@' not in user_mail and '.' not in user_mailbox:
+    print('Это вообще не почта ')
+elif user_mailbox in free_mailbox:
+    print('Это почта, она на бесплатном домене')
+else:
+    print('Это почта, она на корпоратином домене')
